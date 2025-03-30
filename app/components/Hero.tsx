b604298c2d7c0340522f2c7e44168bcca288e00f
UmeshKumar0143 <umeshkumar153654@gmail.com>
2025-03-30 15:29:10 +0530

    About Sections

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,11 @@
 export default function Hero(){
-    return <div className="w-full h-screen">
+    return <div className="w-full">
         <div className=" w-full container mt-16  grid grid-cols-3  mx-auto h-[35vw] ">
             <div className="  container px-10 py-8 col-span-2 -red-400">
                 <h1 className="text-white text-[4.5vw]">Hi, I'm </h1>
                 <h1 className="text-[5.4vw]  leading-10 tracking-tighter text-purple-500 font-medium " >Umesh Kumar</h1>
-                <p className="text-white mt-10 ml-2 text-lg ">A  <span className="text-purple-400 font-medium"> Full-Stack Developer</span>from India </p>
-                <p className="text-white  ml-2 capitalize">bridging technology and creativity through <span className="text-purple-400 font-medium">web development</span>.</p>
+                <p className="text-white mt-10 ml-2 text-lg ">A  <span className="text-purple-400 font-medium"> Full-Stack Developer </span>from India </p>
+                <p className="text-white  ml-2 mt-2 ">Bridging Technology and Creativity through <span className="text-purple-400 capitalize ml-1 font-medium">web development</span>.</p>
                 <div className="flex w-full mt-12 gap-20">
                     <button className="text-white font-medium hover:text-purple-400  hover:cursor-pointer  px-8 py-2 border-2 border-purple-400 rounded-full">Hire me</button>
                     <button className="text-white font-medium hover:text-purple-400  hover:cursor-pointer  px-8 py-2 border-2 border-purple-400 rounded-full">Resume</button>
@@ -13,7 +13,6 @@ export default function Hero(){
             </div>
             <div className="container py-20">
                 <div className="w-full -mt-18  bg-purple-400 h-full rounded-full  ">
-
                 </div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navabar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function Home() {
   return (
    <div className=" w-full h-screen ">
     <Navbar/>
     <Hero/>
+    <About/>
    </div>
   );
 }
