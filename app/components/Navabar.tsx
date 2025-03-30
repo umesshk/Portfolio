c3e3ad33e80eba16eb078d06c0ad3ee5510ba44f
UmeshKumar0143 <umeshkumar153654@gmail.com>
2025-03-30 15:00:39 +0530

    Hero SEctoin

--- app/components/Navabar.tsx
+++ app/components/Navabar.tsx
@@ -5,8 +5,8 @@ const NavItems = ["Home", "About", "Skills", "Projects", "Contact"]
 export default function Navbar(){
     return <div className="container py-10 w-full mx-auto gap-10 items-center   flex justify-between ">
             <div className="bg-purple-500 h-1 w-full rounded-xl"></div>
-            <div className="flex gap-16 ">{NavItems.map((item: string,inex: number)=>{
-                return <span className="text-white text-xl hover:text-purple-400 hover:cursor-pointer tracking-tight ">{item}</span>
+            <div className="flex gap-16 ">{NavItems.map((item: string,index: number)=>{
+                return <span key={index} className="text-white text-xl hover:text-purple-400 hover:cursor-pointer tracking-tight uppercase ">{item}</span>
             })}</div>
             <div className="bg-purple-500 h-1 rounded-xl w-full"></div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/Navabar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-   <div className=" w-full h-screen bg-gradient-to-r from-black to-gray-900 ">
+   <div className=" w-full h-screen ">
     <Navbar/>
+    <Hero/>
    </div>
   );
 }
